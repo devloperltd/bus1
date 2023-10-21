beb5b60bd6592ffbdac5def9dcda4b50574e6dd7
Laroussi Boulanouar <94921918+devloperltd@users.noreply.github.com>
2023-10-21 22:13:46 +0100

    Add files via upload

---
+++ bus.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk
+
+bus = Tk()
+bus.title('Application Gestion De Location De Bus')
+titre = Label(bus, text="Gestion De Location De Bus")
+titre.pack()
+
+bus.config(background="#dcdde1")
+bus.geometry("900x300")
+
+
+
+
+bus.mainloop()
---
+++ static.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk
+
+
+static = Tk()
+static.title('Application Gestion De Location De Bus')
+titre = Label(static, text="Gestion De Location De Bus")
+titre.pack()
+
+static.config(background="#dcdde1")
+static.geometry("900x300")
+
+
+
+
+static.mainloop()
